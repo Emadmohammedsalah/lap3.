d6c8226a1d28a9be2f7859098e665f39b5e025dc
Emadmohammedsalah <111250396+Emadmohammedsalah@users.noreply.github.com>
2022-08-28 21:12:32 +0300

    Add files via upload

---
+++ untitled/src/Q11.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Q11 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+     final    int x=9;
+
+        System.out.println();
+    }
+}
